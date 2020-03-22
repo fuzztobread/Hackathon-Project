3b9087a0932ded2e34292821a52f1a11893834f3
Rushab Humagain <humagainrushab@gmail.com>
2020-03-22 15:43:20 +0545

    Styles added

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask,render_template,request
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
+
+
+def index():
+    if request.method == "GET":
+        pass
+    if request.method == "POST":
+        pass
+
+
+    return render_template("index.html")
+
